3b2d3d646104734d7f616bad0254413506671fbb
ronan <hackronan90@gmail.com>
2020-05-05 17:21:05 +0700

    new

--- bot2.py
+++ bot2.py
@@ -2,9 +2,18 @@ import requests, json
 
 def user_info(username):
     data = requests.get('https://api.github.com/users/{}'.format(username))
-    return data.text
+    res = json.loads(data.text)
+    return res
 
+username = user_info('ronank7z')['login']
 
-res = json.loads(user_info('ronank7z'))
+def commit(username, repos):
+    data = requests.get('https://api.github.com/repos/{}/{}/commits'.format(username, repos))
+    res = json.loads(data.text)
+    return res
+
+res = user_info('ronank7z')
+count_commit = len(commit('ronank7z','testbot'))
 
 print(res['login'])
+print(count_commit)
